da7a16f211fb5b92015e433ba591022b8fa5cfae
Эдуард Гумен <hydrospirt@yandex.ru>
2023-02-14 14:30:18 +0300

    Add OwnerViewSet

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from cats.models import Cat
-from cats.serializers import CatSerializer
+from cats.models import Cat, Owner
+from cats.serializers import CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
