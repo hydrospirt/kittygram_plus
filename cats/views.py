f869f9a99b5115206773658bc7c616698832482d
Эдуард Гумен <hydrospirt@yandex.ru>
2023-02-16 12:23:41 +0300

    Add get_serializer_class in CatViewSet

--- cats/views.py
+++ cats/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from cats.models import Cat, Owner
-from cats.serializers import CatSerializer, OwnerSerializer
+from cats.serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
@@ -15,6 +15,11 @@ class CatViewSet(viewsets.ModelViewSet):
         cats = Cat.objects.filter(color='White')[:5]
         serializer = self.get_serializer(cats, many=True)
         return Response(serializer.data)
+
+    def get_serializer_class(self):
+       if self.action == 'list':
+           return CatListSerializer
+       return CatSerializer
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
