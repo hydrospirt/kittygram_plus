672d07e3bc8c01ceee4b8c76c7395857e5fbd00d
Эдуард Гумен <hydrospirt@yandex.ru>
2023-02-16 12:16:27 +0300

    Add recent_white_cats

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from cats.models import Cat, Owner
 from cats.serializers import CatSerializer, OwnerSerializer
@@ -7,6 +9,12 @@ from cats.serializers import CatSerializer, OwnerSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        cats = Cat.objects.filter(color='White')[:5]
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
